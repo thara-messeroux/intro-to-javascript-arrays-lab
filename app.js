a0b2df2f31b7b666fd454c40ce9e68d49d363c9a
thara-messeroux <tharamesseroux@gmail.com>
2025-11-23 21:43:37 -0500

    Exercise 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('sanity check')
+console.log('code works')
+
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+const foods = [];
+
+console.log('Exercise 1 result:', foods);
